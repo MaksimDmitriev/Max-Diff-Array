7377705d3fa9661e44b995639f20fa565d4388c8
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2015-04-09 00:30:24 +0300

    Add the algorithm which runs at O(n)

--- src/algo/mindiff/Main.java
+++ src/algo/mindiff/Main.java
@@ -5,9 +5,14 @@ public class Main {
     // http://www.careercup.com/question?id=5185822661804032
     // http://codereview.stackexchange.com/q/85857/23821
     public static void main(String[] args) {
-        System.out.println(minDiff(new int[] { 10, 1, 12, 3, 4, 28, 1 }));
+        System.out.println(minDiffLinear(new int[] { 10, 1, 12, 3, 4, 28, 1 }));
     }
 
+    /*
+     * This solution is mine. When I was implementing it, I was sure it was O(n).
+     * O(n) - best case
+     * O(n2/2) - worst case
+     */
     private static int minDiff(int[] array) {
         int diff = 0;
         int result = 0;
@@ -40,6 +45,25 @@ public class Main {
             }
         }
         return max;
+    }
+
+    /*
+     * O(n)
+     * I'm thankful to @Comintern who replied to me on
+     * http://codereview.stackexchange.com/a/85875/23821
+     */
+    private static int minDiffLinear(int[] array) {
+        int min = array[0];
+        int diff = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+            if (array[i] - min > diff) {
+                diff = array[i] - min;
+            }
+        }
+        return diff;
     }
 
 }
